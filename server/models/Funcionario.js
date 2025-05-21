0753c5ca02497965a65f9051e8e3e0be6ae92f17
pasteldemisto <jdsouz@edu.unifor.br>
2025-05-21 17:41:57 -0300

    perfil-gerente.js feito

--- server/models/Funcionario.js
+++ server/models/Funcionario.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 const funcionarioSchema = new mongoose.Schema({
-    name: {type:String, required:true},
+    nome: {type:String, required:true},
     cargo: {type:String, required:true},
     matricula: {type:String, required:true, unique:true},
     email: {type:String, required:true},
-    password: {type:String, required:true}
+    senha: {type:String, required:true}
 });
 
 
