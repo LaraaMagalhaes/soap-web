7ac51f610eeb24b8a02ab349bcb90f1cff776c22
LaraaMagalhaes <annalarammv123@gmail.com>
2025-06-02 17:57:48 -0300

    .

--- client/assets/js/home-gerente.js
+++ client/assets/js/home-gerente.js
@@ -78,6 +78,10 @@ document.addEventListener('DOMContentLoaded', async () => {
   document.getElementById('botao-perfil').addEventListener('click', () => {
     window.location.href = 'perfil-gerente.html';
   });
+document.getElementById('menu-toggle-btn').addEventListener('click', () => {
+  document.getElementById('nav-links').classList.toggle('active');
+});
+
 
   await carregarTarefasDoDia();
   if (registros.length > 0) {
